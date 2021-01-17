449d50bb7e93ff46a15396deda01280a72cdf3ea
Florian Tönjes <skatepainter@yahoo.de>
2021-01-17 18:53:07 +0100

    Created BaseRepository and interface

---
+++ OnlineForum/Data/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineForum.Data
+{
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        protected Context Context { get; private set; }
+
+        public BaseRepository(Context context)
+        {
+            this.Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = Context.Find<TEntity>(id);
+            Context.Remove(entity);
+            Context.SaveChanges();
+        }
+
+        public TEntity Get(int id)
+        {
+            return Context.Find<TEntity>(id);
+        }
+
+        public IList<TEntity> GetList()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ OnlineForum/Data/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineForum.Data
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+
+        IList<TEntity> GetList();
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(int id);
+    }
+}
