645fc17ac3759a87733bd58f6a3ca6af9b11d07e
Florian Tönjes <skatepainter@yahoo.de>
2021-01-17 12:15:35 +0100

    Context Klasse erstellt

---
+++ OnlineForum/Data/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineForum.Models;
+
+namespace OnlineForum.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Board> Boards { get; set; }
+
+        public DbSet<Thread> Threads { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<PrivateMessage> PrivateMessages { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserSettings> UserSettings { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
